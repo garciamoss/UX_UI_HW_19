2d8e88b45b42d11c5e65ed51de6aec98d80be97c
garciamoss <lisa@spanishmosscreative.net>
2022-05-14 20:16:27 -0400

    Finally—a jQuery I like!
    Something simple, but effective. Just click on my self-portrait…

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log("Your index.js file is loaded correctly");
 
-$("#heroImg").on("click", function(){
-    console.log("You clicked on my #ID");
-});
+$("#heroImg").click(function(){
+    $("p").slideToggle("slow");
+  });
